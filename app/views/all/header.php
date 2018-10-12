121b60f654d38e9d27915e0386ff40bea5daef00
Felipe <affernandez74@misena.edu.co>
2018-10-12 11:22:52 -0500

    Style Cambio de tipo de familia de letra.

--- app/views/all/header.php
+++ app/views/all/header.php
@@ -12,5 +12,12 @@
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/app.css">
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/emple.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <style>
+    *{
+      font-family: 'Indie Flower', cursive;
+    }
+    
+    </style>
   </head>
   <body>
